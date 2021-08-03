dda2dd82b1ad130b74fa0d64a90025b3a114c1cc
lihong <1091262281@qq.com>
2021-08-03 19:14:58 +0800

    fix: 修复部署bug

--- web/pages/index/fetch.ts
+++ web/pages/index/fetch.ts
@@ -10,7 +10,7 @@ export default async (
 ) => {
 	// 阅读文档获得更多信息 http://doc.ssr-fc.com/docs/features$fetch#%E5%88%A4%E6%96%AD%E5%BD%93%E5%89%8D%E7%8E%AF%E5%A2%83
 	console.log('window', window);
-	const data = __isBrowser__ ? await (await window.fetch('/api/index')).json() : await ctx.apiService?.getFile({data: '23'});
+	const data = __isBrowser__ ? await (await window.fetch('/api/index')).json() : await ctx.apiService?.getFile?.();
 	return {
 		// 建议根据模块给数据加上 namespace防止数据覆盖
 		indexData: { fileList: data }
